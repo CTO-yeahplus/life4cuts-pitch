d88bff8ec585e442b9967e4a172c39dc88bb4db3
“Eugene” <cto@yeahplus.co.kr>
2026-03-21 17:11:56 +0900

    updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: '엘케이벤처스 IR ',
   description: '공간 비즈니스의 퀀텀 점프. 데이터로 증명된 상위 1%의 오프라인 공간 수익화 전략, 글로벌 수퍼앱 고마진 전략',
   openGraph: {
-    title: '[Confidential] 인생네컷 PLUS VVIP 제안서',
+    title: '[Confidential] 엘케이벤처스 VVIP 제안서',
     description: '가장 확실한 상권이 남아있는 지금, 수익이 폭발하는 복합 문화 플랫폼에 합류하십시오.',
     url: 'https://life4cuts-pitch.vercel.app', // 나중에 실제 배포하실 도메인 주소로 변경하세요
     siteName: 'LK Ventures Global',
